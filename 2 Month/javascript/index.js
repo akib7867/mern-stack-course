22a4444b55d695a7808cf0c5b03587c05b43b974
akib7867 <akibjavedalase@gmail.com>
2025-06-11 11:44:42 +0530

    Done

---
+++ 2 Month/javascript/index.js
@@ -0,0 +1,9 @@
+
+
+function writefullname(firstname,secondname,lastname) {
+
+    const fullname = firstname + secondname + lastname ;
+    console.log(fullname)
+
+}
+writefullname( "Akib ", "Javed ", "Alase ")
