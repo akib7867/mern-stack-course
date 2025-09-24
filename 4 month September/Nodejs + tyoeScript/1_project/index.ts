d3087e5730dbd99d3203a2f29005ae3069c36eb6
akib7867 <akibjavedalase@gmail.com>
2025-09-24 21:40:27 +0530

    Done

---
+++ 4 month September/Nodejs + tyoeScript/1_project/index.ts
@@ -0,0 +1,20 @@
+// Temple literals- String Temple , Temple string ,litrals
+
+const userName : String = "my name is Akib"
+
+console.log("My name is" + userName )
+
+console.log('my name is  ${userName}')
+
+
+// Rest Oprators
+
+const firstArray :Number[] = [10,20,30,40]
+
+const secoundArray : Number[] = [52,53,54,55]
+
+   // 10,20,30,40,52,53,54,55 = show the terminal
+
+
+
+//  spread Oprators
